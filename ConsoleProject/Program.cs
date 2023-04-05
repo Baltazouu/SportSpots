a254115e10d29513bb6056224465385e55b2ffa5
Baptiste DUDONNE <badudonne>
2023-04-05 12:01:42 +0200

    ajout 1st class

---
+++ ConsoleProject/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Maui1;
+
+Console.WriteLine("Hello, World!");
+
+Spot a = new Spot("a","b","adresse","nom");
+
+Spot empty = new Spot();
+
+Console.WriteLine(a);
+
+Console.WriteLine(empty);
+
+
---
+++ Maui1/Spot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Specialized;
+
+namespace Maui1
+{
+    // All the code in this file is included in all platforms.
+    public class Spot
+    { 
+
+        public string Numero { get; set; }
+        public string Nom { get; set; }
+        public string Famille { get; set; }
+        public string Type { get; set; }
+        public string Adresse { get; set; }
+        int CodePostal {get; set;}
+        public string Coord_x { get; set; }
+        public string Coord_y { get; set; }
+        public bool accessibilite { get; set; }
+        public bool restauration { get; set; }
+
+        public bool public_access { get; set; }
+
+
+
+        public Spot(string coord_x, string coord_y, string adresse,string nom)
+        {
+            Coord_x=coord_x;
+            Coord_y=coord_y;
+            Adresse=adresse;
+            Nom = nom;
+        }
+
+        public Spot()
+        { }
+
+         public override string ToString()
+        {
+            return "Spot : " + Nom + " Adresse " + CodePostal;
+        }
+
+
+    }
+}
